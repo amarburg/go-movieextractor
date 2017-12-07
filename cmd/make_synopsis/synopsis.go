71fe803ed96bc9c664f113a30eec860a78780432
Aaron Marburg <amarburg@apl.washington.edu>
2017-12-06 16:09:18 -0800

    Moved make_proof_sheet to go-frameset

---
+++ cmd/make_synopsis/synopsis.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	//"flag"
+	"github.com/amarburg/go-multimov"
+	"log"
+	// "fmt"
+	"html/template"
+	// "image"
+	// "image/png"
+	// "math"
+	// "os"
+	"github.com/spf13/cobra"
+	"os"
+	"path/filepath"
+)
+
+//var templateFile string
+
+var cmdSynopsis = &cobra.Command{
+	Use:   "synopsis [image set file]",
+	Short: "Generates a synopsis page sheet from an image set",
+	Long:  "a long string explaining the command and giving usage information",
+	Args:  cobra.MinimumNArgs(1),
+	Run:   runSynopsis,
+}
+
+func init() {
+	//cmdProofSheet.PersistentFlags().StringVar(&templateFile, "template", filepath.Join( DefaultTemplateDir(), "synopsis.tmpl"), "Template file to use")
+	//cmdProofSheet.PersistentFlags().Uint64Var(&step, "step", uint64(math.Trunc(29.97*60)), "")
+}
+
+func runSynopsis(cmd *cobra.Command, args []string) {
+
+	outdir := OutputDir
+
+	source := args[0]
+
+	set, err := multimov.LoadFrameSet(source)
+	if err != nil {
+		log.Fatalf("Unable to load FrameSet from \"%s\": %s", source, err)
+	}
+
+	// Construct multimov from image set
+	multimovPath := os.ExpandEnv(set.Source)
+	mm, err := multimov.LoadMultiMov(multimovPath)
+	if err != nil {
+		log.Fatalf("Unable to load MultiMov from \"%s\": %s", multimovPath, err)
+	}
+
+	set.NumFrames = mm.NumFrames()
+
+	outTree := NewDirOutTree(outdir)
+	im := NewImageMaker(mm, outTree)
+
+	indexfile := outTree.join("index.html")
+	outfile, err := os.Create(indexfile)
+	if err != nil {
+		log.Fatalf("Unable to open the output file \"%s\": %s", indexfile, outfile)
+	}
+	defer outfile.Close()
+
+	// Funtion map
+	fmap := template.FuncMap{
+		"makeImages": im.MakeImages,
+		"frameSetName": func(set multimov.FrameSet) string {
+			return filepath.Base(set.Source)
+		},
+		"sampleFramesFromChunk": sampleFramesFromChunk,
+	}
+
+	t := template.New("synopsis.tmpl")
+	t = t.Funcs(fmap)
+
+	// Load layouts
+	t = template.Must(t.ParseGlob(filepath.Join(DefaultTemplateDir(), "layouts/*.tmpl")))
+	t = template.Must(t.ParseFiles(filepath.Join(DefaultTemplateDir(), "synopsis.tmpl")))
+
+	err = t.Execute(outfile, set)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func sampleFramesFromChunk(chunk multimov.NamedChunk) []uint64 {
+	out := []uint64{}
+
+	out = append(out, chunk.Range.Start, chunk.Range.End)
+
+	return out
+}
--- frameset.go
+++ frameset.go
@@ -1,11 +1,77 @@
-package FrameSet
+package multimov
+
+import (
+	"encoding/json"
+	"os"
+	"sort"
+)
 
 type FrameSet struct {
 	Source    string
 	Frames    []uint64
+	Chunks    map[string]Chunk `json:",omitempty"`
 	ImageName string
-	Chunks map[string][]uint64
-	// Chunks    map[string]struct {
-	// 	Frames []uint64
-	// }
+	NumFrames uint64 `json:",omitempty"`
+}
+
+type Chunk struct {
+	Range FrameRange
+}
+
+type FrameRange struct {
+	Start uint64
+	End   uint64 `json:",omitempty"`
+}
+
+// LoadMultiMov reads a MultiMov from the a path to a given JSON file.
+// Returns a pointer to a new MultiMov if successful, or nil and
+// an error if unsuccessful
+func LoadFrameSet(path string) (*FrameSet, error) {
+	file, err := os.Open(path)
+	defer file.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	decoder := json.NewDecoder(file)
+	set := new(FrameSet)
+	err = decoder.Decode(set)
+
+	return set, err
+}
+
+type NamedChunk struct {
+	Chunk
+	Key string
+}
+
+type NamedChunkSlice []NamedChunk
+
+func (p NamedChunkSlice) Len() int           { return len(p) }
+func (p NamedChunkSlice) Less(i, j int) bool { return p[i].Range.Start < p[j].Range.Start }
+func (p NamedChunkSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+func (set FrameSet) OrderedChunks() NamedChunkSlice {
+	out := make(NamedChunkSlice, 0, len(set.Chunks))
+
+	for key, chunk := range set.Chunks {
+		out = append(out, NamedChunk{Chunk: chunk, Key: key})
+	}
+
+	sort.Sort(out)
+
+	// Fill out end ranges
+	for i := 0; i < len(out); i++ {
+		if out[i].Range.End == 0 {
+
+			if i == len(out)-1 {
+				out[i].Range.End = set.NumFrames
+			} else {
+				out[i].Range.End = out[i+1].Range.Start
+			}
+		}
+	}
+
+	return out
 }
