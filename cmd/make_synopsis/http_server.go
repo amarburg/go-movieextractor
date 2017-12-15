6d02a91ea2843423dbe9fa3cc464d079b0c078bb
Aaron Marburg <amarburg@apl.washington.edu>
2017-12-14 20:30:12 -0800

    Removed some cruft from http_server.

--- cmd/make_synopsis/http_server.go
+++ cmd/make_synopsis/http_server.go
@@ -14,10 +14,7 @@ var cmdHttpServer = &cobra.Command{
 	Run:   runHttpServer,
 }
 
-func init() {
-	//cmdProofSheet.PersistentFlags().StringVar(&templateFile, "template", filepath.Join( DefaultTemplateDir(), "synopsis.tmpl"), "Template file to use")
-	//cmdProofSheet.PersistentFlags().Uint64Var(&step, "step", uint64(math.Trunc(29.97*60)), "")
-}
+//func init() {}
 
 func runHttpServer(cmd *cobra.Command, args []string) {
 
@@ -27,54 +24,4 @@ func runHttpServer(cmd *cobra.Command, args []string) {
 
 	http.Handle("/", http.FileServer(http.Dir(outdir)))
 	log.Fatal(http.ListenAndServe(":9080", nil))
-
-	// source := args[0]
-  //
-	// set, err := frameset.LoadFrameSet(source)
-	// if err != nil {
-	// 	log.Fatalf("Unable to load FrameSet from \"%s\": %s", source, err)
-	// }
-  //
-	// // Construct multimov from image set
-	// multimovPath := os.ExpandEnv(set.Source)
-	// mm, err := multimov.LoadMultiMov(multimovPath)
-	// if err != nil {
-	// 	log.Fatalf("Unable to load MultiMov from \"%s\": %s", multimovPath, err)
-	// }
-  //
-	// set.NumFrames = mm.NumFrames()
-  //
-	// outTree := NewDirOutTree(outdir)
-	// im := NewImageMaker(mm, outTree)
-  //
-	// indexfile := outTree.join("index.html")
-	// outfile, err := os.Create(indexfile)
-	// if err != nil {
-	// 	log.Fatalf("Unable to open the output file \"%s\": %s", indexfile, outfile)
-	// }
-	// defer outfile.Close()
-  //
-	// // Funtion map
-	// fmap := template.FuncMap{
-	// 	"makeImages": im.MakeImages,
-	// 	"makeScrubNails": func(chunk frameset.Chunk) []ScrubNail {
-	// 		return makeScrubNails(im, chunk)
-	// 	},
-	// 	"frameSetName": func(set frameset.FrameSet) string {
-	// 		return filepath.Base(set.Source)
-	// 	},
-	// }
-  //
-	// t := template.New("synopsis.tmpl")
-	// t = t.Funcs(fmap)
-  //
-	// // Load layouts
-	// t = template.Must(t.ParseGlob(filepath.Join(DefaultTemplateDir(), "layouts/*.tmpl")))
-	// t = template.Must(t.ParseFiles(filepath.Join(DefaultTemplateDir(), "synopsis.tmpl")))
-  //
-	// err = t.Execute(outfile, set)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
 }
