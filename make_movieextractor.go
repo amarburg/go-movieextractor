e5a4b5a8bcf59e5ce7e408486839dc72d2873b3a
Aaron Marburg <amarburg@apl.washington.edu>
2017-12-14 22:30:30 -0800

    Moved MovieExtractor abstraction to multimov.

--- make_movieextractor.go
+++ make_movieextractor.go
@@ -4,47 +4,9 @@ import (
 	"fmt"
 	"github.com/amarburg/go-lazyquicktime"
 	"github.com/amarburg/go-multimov"
-	"github.com/amarburg/go-lazyfs"
 	"os"
 	"path/filepath"
 )
-
-
-// Technically this could be in multimov?
-func MovieExtractorFromPath(path string) (lazyquicktime.MovieExtractor, error) {
-
-	if filepath.Ext(path) == ".mov" {
-
-		file, err := lazyfs.SourceFromPath(path)
-
-		if err != nil {
-			return nil, err
-		}
-
-		qtInfo, err := lazyquicktime.LoadMovMetadata(file)
-
-		if err != nil {
-			return nil, err
-		}
-
-		return qtInfo, nil
-
-	} else if filepath.Ext(path) == ".json" {
-
-		fmt.Println(path)
-
-		mm, err := multimov.LoadMultiMov(path)
-		if err != nil {
-			return nil, err
-		}
-
-		return mm, nil
-
-	}
-
-	return nil, fmt.Errorf("Can't make a movie extractor from file %s", path)
-
-}
 
 
 func (set FrameSet) MovieExtractor() (lazyquicktime.MovieExtractor, error) {
@@ -60,7 +22,7 @@ func (set FrameSet) MovieExtractor() (lazyquicktime.MovieExtractor, error) {
 		source = filepath.Clean(filepath.Join(filepath.Dir(set.filepath), source))
 	}
 
-	return MovieExtractorFromPath( source )
+	return multimov.MovieExtractorFromPath( source )
 
 
 }
