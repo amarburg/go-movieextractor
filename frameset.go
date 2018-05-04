94c0dbb1fdd6bf45c72f656c26d418e6eaac64ad
Aaron Marburg <amarburg@apl.washington.edu>
2018-05-04 12:33:10 -0700

    Squashed frameset_movie_extractor into frameset.go

--- frameset.go
+++ frameset.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"sort"
+	"path/filepath"
 )
 
 type FrameSet struct {
@@ -134,4 +135,24 @@ func (fs *FrameSet) MovFromChunk(name string) (VirtualMov, error) {
 	}
 
 		return VirtualMov{}, fmt.Errorf("Couldn't find chunk \"%s\"", name )
+}
+
+
+
+func (set FrameSet) MovieExtractor() (MovieExtractor, error) {
+	// Create the source
+
+	source := os.ExpandEnv(set.Source)
+
+	// If path is relative, evaluate it relative to the input file...
+	if !filepath.IsAbs(set.Source) {
+		if len(set.filepath) == 0 {
+			return nil, fmt.Errorf("Source path in frameset is relative, but don't know filepath for frameset")
+		}
+
+		source = filepath.Clean(filepath.Join(filepath.Dir(set.filepath), source))
+	}
+
+	return MovieExtractorFromPath(source)
+
 }
